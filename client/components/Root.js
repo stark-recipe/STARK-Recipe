73b722c747293be306112e697ce753b3d0d9d686
Jaime Gonnzalez <2016jgonzalez139@gmail.com>
2019-05-01 18:53:53 -0700

    more router functionality

--- client/components/Root.js
+++ client/components/Root.js
@@ -3,24 +3,21 @@ import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import AuthPage from '../containers/AuthPage.jsx';
-import SignupPage from '../containers/SignupPage.jsx';
-import MainContainer from '../containers/MainContainer.jsx';
 import { Switch } from 'react-router';
 
+import SignupPage from '../containers/SignupPage.jsx';
+import MainContainer from '../containers/MainContainer.jsx';
+import SelectedCardDisplay from '../containers/SelectedCardDisplay.jsx'
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
-<<<<<<< HEAD
-      <Route exact path="/test" component={AuthPage} />
-      <Route exact path="/" component={MainContainer} />
-=======
       <Switch>
         <Route exact path="/" component={AuthPage} />
         <Route path="/signup" component={SignupPage} />
         <Route path="/maincontainer" component={MainContainer} />
+        <Rout path="/selectedCard" component={SelectedCardDisplay} />
       </Switch>
->>>>>>> fa03b9caf23776adb083bad570014dd727d2dd24
     </Router>
   </Provider>
 )
