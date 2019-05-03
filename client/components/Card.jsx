cd402e41b57db28318a2c086a347237bfce2ae73
Jaime Gonnzalez <2016jgonzalez139@gmail.com>
2019-05-02 17:23:56 -0700

    added css transitions;

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = props => {
   return (
     <div className="card">
-      <button onClick={(e) => {props.onCardClicked(props.index)}}>
+      <button  className="cardButton" onClick={(e) => {props.onCardClicked(props.index)}}>
         <img src={props.info.image} />
         <h4>{props.info.label}</h4>
         <p>calories: {props.info.calories}</p>
